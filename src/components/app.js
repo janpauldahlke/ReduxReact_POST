ae6b0b348a8be31f109a64b76be1e09aa1cb742f
janpauldahlke <janpauldahlke@gmail.com>
2017-03-27 11:41:08 +0200

    testing nested routes

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>entering App ...</div>
+      <div>entering App ...
+        { this.props.children }
+      </div>
     );
   }
 }
+
+/* TODO
+  - apply nested route into App, by calling it with {this.props.children}
+
+ */
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,16 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
-//map route and component
+//map route and component here // export them, give atribs to component in index!
+
+const Test = () => {
+  return <div>Hi routes Test</div>;
+};
+
 export default (
-  <Route path="/" component={ App } />
+  <Route path="/" component={ App } >
+    <Route path="test" component={ Test } />
+    <Route path="test2" component={ Test } />
+    <Route path="test3" component={ Test } />
+  </Route>
 );
