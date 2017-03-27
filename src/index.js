7e99d34c217c7d697def220bff04a11ddf8cbb73
janpauldahlke <janpauldahlke@gmail.com>
2017-03-27 11:21:09 +0200

    preparing route to /

--- src/index.js
+++ src/index.js
@@ -2,3 +2,33 @@
 console.log('scio non scire');
 
 //
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+// www.foo.com/<browserHistoryObject>
+// alternative { hashHistory } --> www.foo.com/#posts/12<starts with hash>
+// alternative { memoryHistory } -->
+import reducers from './reducers';
+import routes from './routes';
+
+
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={ createStoreWithMiddleware(reducers) }>
+    <Router history={ browserHistory } routes={ routes } />
+  </Provider>
+  , document.querySelector('#root')
+);
+
+
+/*
+TODO:
+  define all possible valid routes //configure them
+  see src/routes.js
+*/
