256739804d27bd2c9320575afec13ce0bdd8496f
janpauldahlke <janpauldahlke@gmail.com>
2017-04-04 10:43:59 +0200

    pre-prepared postView

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 //map route and component here // export them, give atribs to component in index!
 
@@ -13,5 +14,11 @@ export default (
   <Route path="/" component={ App } >
     <IndexRoute component={ PostsIndex } />
     <Route path="posts/new" component={ PostsNew } />
+    <Route path="posts/:id" component={ PostsShow } />
   </Route>
 );
+
+
+//TODO
+// <Route path="post:/id" make :id accessible
+// this.props.param.id (whereby param comes with URL)
