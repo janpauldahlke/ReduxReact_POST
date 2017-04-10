dd2c7d53b4805531d08aea955635d34e2e0b0908
janpauldahlke <janpauldahlke@gmail.com>
2017-04-10 10:00:51 +0200

    delete post and context.route back to index

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
 import { Link } from 'react-router';
+
 
 class PostsShow extends Component {
 
+  // context() usage
+  static contextTypes = {
+    router : PropTypes.object
+  };
+
+
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeleteClick () {
+                    //id on this.props.params.id
+    this.props.deletePost(this.props.params.id)
+      .then(() => {
+        //navigate back to index
+        //after the action
+        // do not forget to import PropTypes and create static contextTypes
+        this.context.router.push('/');
+      });
   }
 
   render() {
@@ -21,10 +39,16 @@ class PostsShow extends Component {
 
     return (
       <div className="post">
-        <Link className="btn btn-primary" to="/">Back to Index</Link>
+        <Link className="btn btn-primary pull-xs-left" to="/">Back to Index</Link>
+        <button className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Post</button>
+        <br></br>
+        <hr></hr>
         <h3>{this.props.post.title}</h3>
         <h6>categories: {this.props.post.categories}</h6>
         <p>{this.props.post.content}</p>
+        <hr></hr>
       </div>
     )
   }
@@ -34,7 +58,7 @@ function mapStateToProps (state) {
   return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
 
 
 //TODO
